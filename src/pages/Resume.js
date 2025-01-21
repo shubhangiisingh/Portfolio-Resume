1142907e8d1dd3f69326b5309b8bfae81f933218
shubhangiisingh <singhshubhangi632@gmail.com>
2025-01-21 23:56:43 +0530

    changes made to app.css, resume.css and resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,16 +5,18 @@ import resumeData from '../utils/resumeData'
 const resume = () => {
   return (
     <>
-    <Grid container className='resume_container'>
-      <Grid item className='section_title'><span></span><Typography variant='h6'>Resume</Typography></Grid>
-      <Grid item xs={12}>
-        <Typography>{resumeData.about}</Typography>
+    <Grid container className='section pb_45'>
+      <Grid item className='section_title mb_30'>
+        <span></span>
+        <h6 className='section_title_text'>Resume</h6></Grid>
+      <Grid item xs={12} >
+        <Typography variant='body2' className='aboutme_text'>{resumeData.about}</Typography>
       </Grid>
     </Grid>
-    <Grid container className='resume_container'></Grid>
-    <Grid container className='resume_container'></Grid>
-    <Grid container className='resume_container'></Grid>
-    <Grid container className='resume_container'></Grid>
+    <Grid container className='section pb_45'></Grid>
+    <Grid container className='section pb_45'></Grid>
+    <Grid container className='section pb_45'></Grid>
+    <Grid container className='section pb_45'></Grid>
     </>
   )
 }
