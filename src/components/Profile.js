8d769376b9d2eface5b9902c8ac0e7cc4939b587
Shubhangi Singh <shubhangisingh0013@gmail.com>
2025-09-12 21:03:16 +0530

    Resume Updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -50,7 +50,7 @@ const Profile = () => {
                     ))}
                 </CustomTimeline>
                 <div className="btn_container">
-                    <a href="/Shubhangi-Resume.pdf" download="Shubhangi-Resume.pdf">
+                    <a href="/Resume_Shubhangi.pdf" download="Shubhangi-Resume.pdf">
                         <CustomButton text={"Download Resume"} icon={<GetAppIcon />} />
                     </a>
                 </div>
